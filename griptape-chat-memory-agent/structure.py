cb94e906f0c2556de82d7c71baa2b07b7dd1f066
Zach Giordano <zgmx22@gmail.com>
2024-09-27 15:09:26 -0400

    Adding rulesets to chat agent

--- griptape-chat-memory-agent/structure.py
+++ griptape-chat-memory-agent/structure.py
@@ -1,4 +1,6 @@
 import os
+from typing import Optional
+import requests
 from dotenv import load_dotenv
 from griptape.structures import Agent
 from griptape.configs import Defaults
@@ -7,6 +9,8 @@ from griptape.drivers import (
 )
 from griptape.drivers import GriptapeCloudEventListenerDriver
 from griptape.events import EventListener, EventBus
+from griptape.rules.rule import Rule
+from griptape.rules.ruleset import Ruleset
 import argparse
 
 
@@ -32,6 +36,46 @@ def get_listener_api_key() -> str:
     return api_key
 
 
+def get_headers():
+    return {
+        "Authorization": f"Bearer {get_listener_api_key()}",
+        "Content-Type": "application/json",
+    }
+
+
+def get_rule_by_id(rule_id: str) -> dict:
+    url = f"{get_base_url()}/api/rules/{rule_id}"
+    response = requests.get(url, headers=get_headers())
+    response.raise_for_status()
+    return response.json()
+
+
+def get_ruleset_by_alias(alias: str) -> dict:
+    url = f"{get_base_url()}/api/rulesets"
+    response = requests.get(url, headers=get_headers(), params={"alias": alias})
+    response.raise_for_status()
+    response_json = response.json()
+    if not response_json["rulesets"]:
+        raise ValueError(f"No ruleset found with alias '{alias}'")
+    return response_json["rulesets"][0]
+
+
+def get_rulesets(ruleset_alias: Optional[str]) -> list[Ruleset]:
+    if ruleset_alias is None:
+        return []
+
+    ruleset = get_ruleset_by_alias(ruleset_alias)
+    rule_ids = ruleset["rule_ids"]
+    rules = [get_rule_by_id(rule_id) for rule_id in rule_ids]
+    griptape_rulesets: list[Ruleset] = []
+    rules_values = []
+    for rule in rules:
+        rules_values.append(Rule(rule["rule"]))
+    griptape_rulesets.append(Ruleset(name=ruleset["name"], rules=rules_values))
+
+    return griptape_rulesets
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -47,6 +91,12 @@ if __name__ == "__main__":
         help="The Griptape Cloud Thread ID you wish to use",
     )
     parser.add_argument(
+        "-r",
+        "--ruleset-alias",
+        default=None,
+        help="Set the ruleset alias to use",
+    )
+    parser.add_argument(
         "-s",
         "--stream",
         default=False,
@@ -57,6 +107,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     prompt = args.prompt
     thread_id = args.thread_id
+    ruleset_alias = args.ruleset_alias
     stream = args.stream
 
     if is_running_in_managed_environment():
@@ -81,6 +132,6 @@ if __name__ == "__main__":
         )
     )
 
-    agent = Agent(stream=stream)
+    agent = Agent(rulesets=get_rulesets(ruleset_alias), stream=stream)
 
     result = agent.run(prompt)
