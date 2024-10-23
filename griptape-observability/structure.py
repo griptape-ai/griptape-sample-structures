e5dae02363a1b8685a75bb499e3de4e0e9d8f90e
cjkindel <cjkindel@users.noreply.github.com>
2024-10-23 17:02:58 -0400

    reformat

--- griptape-observability/structure.py
+++ griptape-observability/structure.py
@@ -8,8 +8,19 @@ from griptape.observability import Observability
 from griptape.structures import Agent
 
 
-def is_running_in_managed_environment() -> bool:
-    return "GT_CLOUD_STRUCTURE_RUN_ID" in os.environ
+def get_structure_run_id() -> str:
+    structure_run_id = os.environ.get("GT_CLOUD_STRUCTURE_RUN_ID", "")
+    if not structure_run_id:
+        print(
+            """
+            ****ERROR****: No value was found for the 'GT_CLOUD_STRUCTURE_RUN_ID' environment variable.
+            This environment variable is required for the GriptapeCloudObservabilityDriver.
+            This structure must be run as a Managed Structure in Griptape Cloud or.
+            with the Skatepark emulator.
+            """
+        )
+        raise EnvironmentError("This script must be run in a Griptape Cloud or Skatepark emulator environment.")
+    return structure_run_id
 
 
 def get_listener_api_key() -> str:
@@ -17,7 +28,7 @@ def get_listener_api_key() -> str:
     if not api_key:
         print(
             """
-            ****WARNING****: No value was found for the 'GT_CLOUD_API_KEY' environment variable.
+            ****ERROR****: No value was found for the 'GT_CLOUD_API_KEY' environment variable.
             This environment variable is required for the GriptapeCloudObservabilityDriver.
             You can generate a Griptape Cloud API Key by visiting https://cloud.griptape.ai/keys .
             Specify it as an environment variable when creating a Managed Structure in Griptape Cloud.
@@ -32,27 +43,26 @@ if __name__ == "__main__":
         "-p",
         "--prompt",
         default="What is the air speed velocity of an unladen swallow?",
-        help="Specify this to prompt the Griptape agent",
+        help="Specify this to run the Griptape agent with a custom prompt",
     )
 
     args = parser.parse_args()
     prompt = args.prompt
 
-    if is_running_in_managed_environment():
-        event_driver = GriptapeCloudEventListenerDriver(api_key=get_listener_api_key())
-        EventBus.add_event_listeners(
-            [
-                EventListener(
-                    # By default, GriptapeCloudEventListenerDriver uses the api key provided
-                    # in the GT_CLOUD_API_KEY environment variable.
-                    driver=event_driver,
-                ),
-            ]
-        )
-    else:
-        raise EnvironmentError("This script must be run in a Griptape Cloud or Skatepark emulator environment.")
+    structure_run_id = get_structure_run_id()
+    api_key = get_listener_api_key()
 
-    observability_driver = GriptapeCloudObservabilityDriver(api_key=get_listener_api_key(), structure_run_id=os.environ["GT_CLOUD_STRUCTURE_RUN_ID"])
+    event_driver = GriptapeCloudEventListenerDriver(api_key=api_key)
+    EventBus.add_event_listeners(
+        [
+            EventListener(
+                # By default, GriptapeCloudEventListenerDriver uses the api key provided
+                # in the GT_CLOUD_API_KEY environment variable.
+                driver=event_driver,
+            ),
+        ]
+    )
 
+    observability_driver = GriptapeCloudObservabilityDriver(api_key=get_listener_api_key(), structure_run_id=structure_run_id)
     with Observability(observability_driver=observability_driver):
         agent = Agent().run(prompt)
