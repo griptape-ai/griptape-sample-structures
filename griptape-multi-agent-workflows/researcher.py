aeb5a4852cef1c1483872564ce21698c3cf27567
Emily Danielson <2302515+emjay07@users.noreply.github.com>
2025-01-13 09:50:17 -0800

    updating multi-agent workflow to 1.1 (#26)
    * updating multi-agent workflow to 1.1
    
    * fixing file name typo
    
    * updating timeout
    
    * updating README on running multi agent structure locally

--- griptape-multi-agent-workflows/researcher.py
+++ griptape-multi-agent-workflows/researcher.py
@@ -4,27 +4,43 @@ import sys
 from griptape.rules import Rule, Ruleset
 from griptape.structures import Agent
 from griptape.tools import (
-    TaskMemoryClient,
-    WebScraper,
-    WebSearch,
+    PromptSummaryTool,
+    WebScraperTool,
+    WebSearchTool,
 )
-from griptape.drivers import GoogleWebSearchDriver
+from griptape.drivers import GoogleWebSearchDriver, GriptapeCloudEventListenerDriver
+from griptape.events import EventBus, EventListener
+
+
+def get_listener_api_key() -> str:
+    api_key = os.environ.get("GT_CLOUD_API_KEY", "")
+    if not api_key:
+        print(
+            """
+              ****WARNING****: No value was found for the 'GT_CLOUD_API_KEY' environment variable.
+              This environment variable is required when running in Griptape Cloud for authorization.
+              You can generate a Griptape Cloud API Key by visiting https://cloud.griptape.ai/keys .
+              Specify it as an environment variable when creating a Managed Structure in Griptape Cloud.
+              """
+        )
+    return api_key
+
 
 def build_researcher():
     """Builds a Researcher Structure."""
     researcher = Agent(
         id="researcher",
         tools=[
-            WebSearch(
+            WebSearchTool(
                 web_search_driver=GoogleWebSearchDriver(
                     api_key=os.environ["GOOGLE_API_KEY"],
                     search_id=os.environ["GOOGLE_API_SEARCH_ID"],
                 ),
             ),
-            WebScraper(
+            WebScraperTool(
                 off_prompt=True,
             ),
-            TaskMemoryClient(off_prompt=False),
+            PromptSummaryTool(),
         ],
         rulesets=[
             Ruleset(
@@ -66,6 +82,15 @@ def build_researcher():
 
     return researcher
 
+
 if __name__ == "__main__":
+    # Set up the EventBus
+    EventBus.add_event_listener(
+        EventListener(
+            event_listener_driver=GriptapeCloudEventListenerDriver(
+                api_key=get_listener_api_key()
+            )
+        )
+    )
     agent = build_researcher()
     agent.run(sys.argv[1])
